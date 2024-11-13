dbe4acbec35f46fa31f93884042beb15a6a97d05
Victor <a27300@svalero.com>
2024-11-13 18:41:51 +0100

    Creacion Repositorio API

---
+++ Models/Constantes.cs
@@ -0,0 +1,9 @@
+namespace CineZarAPI.Models;
+
+public class Constantes
+{
+    public const int Columnas = 5;
+    public const int Filas = 5;
+    public const string CorreoEntradas = "";//"CORREO";
+    public const string PasCorreoEntradas = "";//"CONTRASEÑA";
+}
