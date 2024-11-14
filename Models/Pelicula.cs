984823dc8768dc1b49c985534fa5bb2fb9857911
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-14 16:19:22 +0100

    Cambio de nombre de rama

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -9,7 +9,7 @@ public class Pelicula
     public int Duracion {get; set;} = 0;
     public string? Portada {get; set;}
     public string? Genero {get; set;}
-    public int Estreno {get; set;}
+    public int Estreno {get; set;} = 0;
 
     private static int Identificador {get; set;} = 0;
     public Pelicula(string pTitulo, string pSinopsis, string pDirector, int pDuracion, string pPortada, string pGenero, int pEstreno)
